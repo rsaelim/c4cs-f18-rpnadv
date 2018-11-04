bd67de3fa46d3add8c3dd673029ce03d7924ae58
Khanin <rsaelim@umich.edu>
2018-11-04 15:48:25 -0400

    added yml file now what

--- rpn.py
+++ rpn.py
@@ -1,32 +1,37 @@
 #!/usr/bin/env python3
 
-def  calculate(arg):
-    stack = []
+import operator
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+
+def  calculate(myarg):
+    stack = list()
     tokens = arg.split()
-    for token in tokens:
+    for token in myarg.split():
         try:
-            stack.append(int(token))
+            token = int(token)
+            stack.append(token)
         except ValueError:
+            function = operators[token]
             val2 = stack.pop()
             val1 = stack.pop()
-            if token == '+':
-                result = val1 + val2
-            if token == '-':
-                result = val1 - val2
-
+            result = function(val1, val2)
             stack.append(result)
 
-    if len(stack) > 1:
-        raise ValueError('Too many arguments on the stack')
-    return stack[0]
+    if len(stack) != 1:
+        raise TypeError('Too many arguments on the stack')
+    return stack.pop()
 
 def main():
     while True:
-        try:
-            result = calculate(input("rpn calc> "))
-            print(result)
-        # # except ValueError:
-            pass
+        result = calculate(input("rpn calc> "))
+        print(result)
 
 if __name__ == '__main__':
     main()
